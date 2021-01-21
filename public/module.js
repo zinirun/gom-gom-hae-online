570c6756f0ae09800c0e9180008fa5773156e8e9
zinirun <heojj97@naver.com>
2021-01-21 23:39:49 +0900

    Update module.js

--- public/module.js
+++ public/module.js
@@ -1,25 +1,23 @@
-function logout(){
-    if (confirm("로그아웃 하시겠어요?") == true) {
-        document.location.href = "/process/logout";
-    } else return;
+function logout() {
+    window.location = '/logout';
 }
 
-function showHelp(){
-    var target = document.getElementById("hide_help");
+function showHelp() {
+    var target = document.getElementById('hide_help');
     target.style.display = 'inline-block';
 }
 
-function hideHelp(){
-    var target = document.getElementById("hide_help");
+function hideHelp() {
+    var target = document.getElementById('hide_help');
     target.style.display = 'none';
 }
 
-function showMade(){
-    var target = document.getElementById("hide_made");
+function showMade() {
+    var target = document.getElementById('hide_made');
     target.style.display = 'inline-block';
 }
 
-function hideMade(){
-    var target = document.getElementById("hide_made");
+function hideMade() {
+    var target = document.getElementById('hide_made');
     target.style.display = 'none';
 }
