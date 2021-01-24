76eda75ff5632f1e963fbc1885dfd88f8e7ed43a
zinirun <heojj97@naver.com>
2021-01-24 21:31:00 +0900

    Update game.instance.ts

--- src/controllers/game/game.instance.ts
+++ src/controllers/game/game.instance.ts
@@ -19,7 +19,11 @@ class GameInstance {
             this.onUsers.push([]);
             this.onWords.push([]);
         }
-        this.dict = fs.readFileSync('./dict/dict.txt').toString().replace(/\r/g, '').split('\n');
+        this.dict = fs
+            .readFileSync(__dirname + '/dict/dict.txt')
+            .toString()
+            .replace(/\r/g, '')
+            .split('\n');
     }
 
     public getInGameUserCounts(): number[] {
